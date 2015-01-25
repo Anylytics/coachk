e7aa9de1ac43ff45835bd90009c4355c88c2164c
Gokul Subramanian <gokuls@palantir.com>
2015-01-25 08:55:21 -0500

    Making Live icon interactive

--- static/js/temperatures.js
+++ static/js/temperatures.js
@@ -115,6 +115,7 @@ ractive.observe({
     {
       poll();
     }
+    tweetRactive.set("isLive", status);
   }
 });
 
--- static/js/tweets.js
+++ static/js/tweets.js
@@ -3,22 +3,29 @@ define([ 'ractive', 'rv!../ractive/tweets', 'jquery'], function ( Ractive, templ
 
 
 tweetRactive = new Ractive({
-  el: 'tweetBoxContents',
+  el: 'tweetBox',
   template: template,
   data: {
     tweets: [],
+    isLive: true,
+    currentTime: "0",
     time: function(utctime)
     {
         time = utctime.split(" ")[3];
         split_time = time.split(":");
         split_time[0] = String(parseInt(split_time[0])-5);
         return split_time[0]+":"+split_time[1];
+    },
+    time2: function(utctime) {
+      var hour = parseInt(utctime.substring(0,2));
+      var minute = utctime.substring(2,4);
+      return String(hour-5)+":"+minute;
     }
   }
 });
 
 tweetRactive.on( 'update', function( event, timebin )  {
-    console.log(timebin);
+    tweetRactive.set("currentTime", timebin);
     $.ajax({
         dataType: "json",
         url: "./tweets/"+timebin,
